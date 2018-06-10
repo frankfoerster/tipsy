e567e6de62c3f30f4c2fb327fd57258c03b9b34b
frankfoerster <github@frankfoerster.com>
2018-06-10 23:41:13 +0200

    update routes

--- config/routes.php
+++ config/routes.php
@@ -17,13 +17,27 @@ Router::scope('/', function (RouteBuilder $routes) {
     // Api routes
     $routes->prefix('api', function(RouteBuilder $routes) {
         $routes->setExtensions(['json']);
+
         $routes->scope('/users', ['controller' => 'Users'], function(RouteBuilder $routes) {
             $routes->connect('/login', ['action' => 'login', '_method' => 'POST']);
             $routes->connect('/logout', ['action' => 'logout', '_method' => 'POST']);
-            $routes->connect('/register', ['action' => 'register', '_method' => 'POST']);
+            $routes->connect('/signup', ['action' => 'signup', '_method' => 'POST']);
             $routes->connect('/lost-password', ['action' => 'lostPassword', '_method' => 'POST']);
             $routes->connect('/reset-password', ['action' => 'resetPassword', '_method' => 'POST']);
+            $routes->connect('/info', ['action' => 'info', '_method' => 'GET']);
+            $routes->connect('/verify', ['action' => 'verify', '_method' => 'POST']);
+            $routes->connect('/request-verification-email', ['action' => 'requestVerificationEmail', '_method' => 'POST']);
         });
+
+        $routes->scope('/tips', ['controller' => 'Tips'], function (RouteBuilder $routes) {
+            $routes->connect('/create-update', ['action' => 'createOrUpdate', '_method' => 'POST']);
+        });
+
+        $routes->scope('/games', ['controller' => 'Games'], function(RouteBuilder $routes) {
+            $routes->connect('/', ['action' => 'index', '_method' => 'GET']);
+        });
+
+        $routes->connect('/*', ['controller' => 'Users', 'action' => 'notFound']);
     });
 
     // App routes
